3ca08186127bf90b18f4df639ebdda2512df040a
Chloe <gtalk.with.nani@gmail.com>
2024-08-21 18:03:41 +0900

    `기지 업 기본 정보` 페이지에 로딩 텍스트 추가

--- pages/base.tsx
+++ pages/base.tsx
@@ -37,42 +37,47 @@ export default function Base() {
         pageImg={`https://lawar.dev1stud.io/og-base.webp?ts=${timestamp}`}
       />
       <h2>기지 업 기본 정보</h2>
-      <div className={styles.table}>
-        <table>
-          <thead>
-            <tr>
-              <th scope="col">기지 레벨</th>
-              <th scope="col">필수 건물업</th>
-              <th scope="col">원래 시간</th>
-              <th scope="col">강철/식량</th>
-              <th scope="col">금화</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row, index) => (
-              <tr
-                key={index}
-                className={selectedRow === index ? styles.current : undefined}
-                onClick={() => handleRowClick(index)}
-              >
-                <td>{row.level}</td>
-                <td>{row.building}</td>
-                <td>{row.time}</td>
-                <td>{row.steel}</td>
-                <td>{row.gold}</td>
+      {data.length > 0 ? (
+        <div className={styles.table}>
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">기지 레벨</th>
+                <th scope="col">필수 건물업</th>
+                <th scope="col">원래 시간</th>
+                <th scope="col">강철/식량</th>
+                <th scope="col">금화</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <ul>
-          <li>원하는 구간을 누르시면 좀 더 편하게 보실 수 있습니다.</li>
-          <li>1 ~ 10레벨 구간은 생략하였습니다.</li>
-          <li>
-            시간은 그 어떤 버프도 받지 않은, 기본 원래 시간 기준입니다. 실제 시간은 테크 연구 상태, VIP, 건설 장관 버프,
-            연맹 지원, 각 건물 파견 생존자, 연맹 센터 상태에 따라 변동이 있을 수 있습니다.
-          </li>
-        </ul>
-      </div>
+            </thead>
+            <tbody>
+              {data.map((row, index) => (
+                <tr
+                  key={index}
+                  className={selectedRow === index ? styles.current : undefined}
+                  onClick={() => handleRowClick(index)}
+                >
+                  <td>{row.level}</td>
+                  <td>{row.building}</td>
+                  <td>{row.time}</td>
+                  <td>{row.steel}</td>
+                  <td>{row.gold}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <p>데이터를 불러오는 중입니다 :)</p>
+      )}
+      <ul>
+        <li>원하는 구간을 누르시면 좀 더 편하게 보실 수 있습니다.</li>
+        <li>1 ~ 10레벨 구간은 생략하였습니다.</li>
+        <li>모든 레벨 공통 필수업 건물인 테크 센터는 공통사항이기 때문에 생략하였습니다.</li>
+        <li>
+          시간은 그 어떤 버프도 받지 않은, 기본 원래 시간 기준입니다. 실제 시간은 테크 연구 상태, VIP, 건설 장관 버프,
+          연맹 지원, 각 건물 파견 생존자, 연맹 센터 상태에 따라 변동이 있을 수 있습니다.
+        </li>
+      </ul>
     </main>
   );
 }
