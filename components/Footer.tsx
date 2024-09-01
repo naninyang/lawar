bf4f2070bd52a1d6cb0afd0a672d6b284d5a763f
Chloe <gtalk.with.nani@gmail.com>
2024-09-01 22:00:27 +0900

    푸터 워딩 변경

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,13 +3,17 @@ import styles from '@/styles/Footer.module.sass';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <p>&copy; 고아리 & 라스트워 482서버 ycl 연맹</p>
       <ul>
-        <li>본 웹사이트는 482서버 ycl 연맹 클로에(본캐 N1W 연맹 빛이나)가 만든 웹사이트입니다.</li>
-        <li>본 웹사이트는 482서버 ycl 연맹에서 사용할 목적으로 제작되었습니다.</li>
         <li>
-          본 웹사이트는 타 서버 또는 타 연맹의 맹원들도 이용이 가능하나, KST 시간대가 아닌 디바이스에서는 일부 기능이
-          오작동 할 수 있습니다.
+          개발 & 디자인: 라스트워 482 [ycl] <strong>클로에</strong> ([N1W] <strong>빛이나</strong>)
         </li>
+        <li>본 웹사이트는 482서버 ycl 연맹과 N1W 일부 연맹원이 사용할 목적으로 제작되었습니다.</li>
+        <li>
+          본 웹사이트는 타 서버 또는 타 연맹의 맹원들도 이용이 가능하나, KST 시간대가 아닌 디바이스에서는 시간대
+          테스트를 진행하지 않았으므로 정상적인 동작을 보장하지 않습니다.
+        </li>
+        <li>한국어 외 다른 언어는 지원할 예정이 없습니다.</li>
       </ul>
     </footer>
   );
