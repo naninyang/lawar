2bea1b690bdcc7db52595051d25402471c480118
Chloe <gtalk.with.nani@gmail.com>
2025-03-16 21:19:00 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/themes',
+        destination: '/general/themes',
+        permanent: true,
+      },
+      {
+        source: '/arms',
+        destination: '/general/arms',
+        permanent: true,
+      },
+      {
+        source: '/showdown',
+        destination: '/general/showdown',
+        permanent: true,
+      },
+      {
+        source: '/base',
+        destination: '/general/base',
+        permanent: true,
+      },
+      {
+        source: '/drone',
+        destination: '/general/drone',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
